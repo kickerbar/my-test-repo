00a8f255443ff3847244031c3bd2508645b68ac3
kickerbar <kicker5782@gmail.com>
2026-06-17 09:38:26 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ variable "ncloud_access_key" { type = string }
 variable "ncloud_secret_key" { type = string }
 variable "my_subnet_no"      { default = "295526" }
 
-# Init Script를 최소화된 형태로 수정
+# 1. 초기화 스크립트 리소스 정의 (들여쓰기 완전 제거)
 resource "ncloud_init_script" "init_script" {
   name    = "ssh-key-init-final"
   content = "#!/bin/bash\nmkdir -p /root/.ssh\necho \"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCBRXlIGdtDw+2LwWzVBjbZu6CFMYG28iJbX0LuAXAWYjOyw2My4lI22JqaaUUL+P6luMaYBoxzCZE74gUBSHtXSV1CyXaAvogmqgBu5453gMLDpLBeFCZejcFWhnlaBLhUjzcNEO2qCjeQVJYv3nx1wV5xmy86pUr90tgs/T80+eI2AS5Yq8qHEc1SLoH3TDzPf++C+lwHaiXcwAef+HB00sWyORmwBM5hIBlFqIvUJn5WwbF1WlopISvJsODRsd6/DrtKfumkJxPpIfwtxbV3s7xsU++Oao3jHBKFYzMs6pSOjIWCQG2eN8nXmL+OBr7h1satO3owPXbj+NzDJ2pT\" >> /root/.ssh/authorized_keys\nchmod 700 /root/.ssh\nchmod 600 /root/.ssh/authorized_keys"
