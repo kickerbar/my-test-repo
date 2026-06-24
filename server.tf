f382b00ca85b69f8c137f5f4f1544a55e2cb2286
kickerbar <kicker5782@gmail.com>
2026-06-24 11:05:26 +0900

    Update server.tf

--- server.tf
+++ server.tf
@@ -1,4 +1,4 @@
-# 1. Init Script 복구 (콘솔에서 지웠으므로 이름 충돌 안 남)
+# 1. Init Script (정상 생성됨)
 resource "ncloud_init_script" "inject_key" {
   name    = "inject-heokey-final"
   os_type = "LNX"
@@ -8,14 +8,16 @@ resource "ncloud_init_script" "inject_key" {
 # 2. 서버 생성
 resource "ncloud_server" "server" {
   name                      = "tf-test-spring"
+  
+  # [추가됨] NIC를 장착하더라도 VPC 환경에서는 기본 서브넷 명시가 필수입니다.
+  subnet_no                 = ncloud_subnet.my_subnet.id
+  
   server_image_product_code = "SW.VSVR.OS.LNX64.ROCKY.0810.B050" 
   server_product_code       = "SVR.VSVR.HICPU.C002.M004.NET.SSD.B050.G002" 
   login_key_name            = "heokey"
-  
-  # 스크립트 연결 (SSH 키 주입용)
   init_script_no            = ncloud_init_script.inject_key.id
   
-  # [핵심] 옛날 코드로 돌아가지 말고, 반드시 NIC를 장착해야 22/80 포트 방화벽이 뚫립니다.
+  # 방금 성공적으로 만든 NIC(방화벽 포함) 장착
   network_interface {
     network_interface_no = ncloud_network_interface.my_nic.id
     order                = 0
